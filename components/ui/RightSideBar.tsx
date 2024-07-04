b77716f2bc8c824622cd4a28fea02cb05017e136
xayuzaii <wizardcodingmachine@gmail.com>
2024-07-04 11:09:57 -0600

    updated data mock

--- components/ui/RightSideBar.tsx
+++ components/ui/RightSideBar.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import Link from "next/link";
+import Image from 'next/image'
 
 const RightSideBar = (props: RightSidebarProps) => {
+  const { user, banks } = props;
+  const{ firstName = '', lastName = '', email = '' } = user;
   return (
     <aside className="right-sidebar">
         <section className="flex flex-col pb-8">
             <div className="profile-banner"/>
+            <div className="profile">
+              <div className="profile-img">
+                <span className="text-5xl font-bold text-blue-500">
+                  {firstName[0] || "X"}
+                </span>
+              </div>
+              <div className="profile-details">
+                <h1 className="profile-name">
+                  {`${firstName} ${lastName}`}
+                </h1>
+                <p className="profile-email">{email}</p>
+              </div>
+            </div>
+        </section>
+        <section className="banks">
+          <div className="flex w-full justify-between">
+            <h2 className="header-2">
+              My Banks
+            </h2>
+            <Link href="/" className="flex gap-2">
+              <Image alt="plus" src="/icons/plus.svg" width={20} height={20}/>
+              <h2 className="text-14 font-semibold text-gray-600">
+                Add bank
+              </h2>
+            </Link>
+          </div>
+          {banks?.length > 0 && (
+            <div className="relative
+              flex
+              flex-1
+              flex-col
+              items-center
+              justify-center
+              gap-5"
+            >
+              <div className="relative z-10">
+                BANK Card 1
+              </div>
+              {banks[1] && (
+                <div className="absolute right-0 top-8 z-0 w-[90%]">
+                  BANK Card 2
+                </div>
+              )}
+            </div>
+          )}
         </section>
     </aside>
   )
