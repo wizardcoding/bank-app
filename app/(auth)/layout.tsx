4bee9e8c14c75ef972088c5e64868fe53cdef745
xayuzaii <wizardcodingmachine@gmail.com>
2024-07-29 19:58:21 -0600

    updated layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <main className="flex ">
+      <main className="columns-2 min-h-screen w-full justify-between font-inter">
           {children}
+          <div className="auth-asset">
+            <div>
+              <Image src="/icons/auth-image.svg" alt="auth" width={500} height={500}/>
+            </div>
+          </div>
       </main>
     );
   }
